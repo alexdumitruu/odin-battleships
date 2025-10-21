4a810adb97edb2e34f85ff7b766e309e15ab5041
alexdumitruu <alex.dumitru.g@gmail.com>
2025-10-21 14:52:08 +0300

    created Ship

---
+++ app.js
@@ -0,0 +1,27 @@
+function Ship() {
+  function createShip(length, nrOfTimesHit = 0, sunk = false) {
+    let ship = {
+      length: length,
+      nrOfTimesHit: nrOfTimesHit,
+      sunk: sunk,
+
+      isSunk: function () {
+        if (this.nrOfTimesHit >= this.length) {
+          this.sunk = true;
+        } else return;
+      },
+
+      hit: function () {
+        if (this.sunk) return;
+        this.nrOfTimesHit++;
+        this.isSunk();
+      },
+    };
+    return ship;
+  }
+  return { createShip, }
+}
+
+function Gameboard() {
+    
+}
